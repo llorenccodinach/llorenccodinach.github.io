dfa41d348913f2654c6e45e1a7cf0abd65d9c80d
llorenccodinach <llorenc.codinach@gmail.com>
2024-09-21 18:53:25 +0200

    h

--- demo-rest-1/rest-1/script.js
+++ demo-rest-1/rest-1/script.js
@@ -131,27 +131,53 @@ svg.addEventListener('touchmove', drag);
 svg.addEventListener('touchend', endDrag);
 svg.addEventListener('touch', endDrag)
 
+let isHovering = false;
+
+const divMaps = document.querySelectorAll(".rest-map-bottom")
+
+divMaps.forEach(element => {
+    element.addEventListener('mouseenter', function() {
+        isHovering = true;
+    });
+    element.addEventListener('mouseleave', function() {
+        isHovering = false;
+    });
+});
+
 document.addEventListener('wheel', function(event) {
-    if (event.deltaY < 0) {
+    if(isHovering){if (event.deltaY < 0) {
         zoom('in',0.1);
     } else if (event.deltaY > 0) {
         zoom('out',0.1)
-    }
+    }}
 });
 
 const mesas = svg.querySelectorAll(".rest-mesa");
+
 console.log(mesas)
 
 mesas.forEach(element => {
     element.addEventListener("pointerdown", () => {
-        inputMesa.value = element.parentElement.getAttribute("xlink:title")
+        const cardMesas = document.querySelectorAll(".rest-emp-right-card");
         if(element.classList.contains("rest-mesa-selected") || element.style.fill == "rgba(128, 0, 0, 0.21)"){
             element.classList.remove("rest-mesa-selected")
+            for (let i = 0; i < cardMesas.length; i++) {
+                cardMesas[i].classList.remove("rest-emp-right-card-selected")
+            }
         }else{
             mesas.forEach(elementP => {
                 elementP.classList.remove("rest-mesa-selected")
             })
             element.classList.add("rest-mesa-selected")
+            inputMesa.value = element.parentElement.getAttribute("xlink:title")
+            for (let i = 0; i < cardMesas.length; i++) {
+                if(element.parentElement.getAttribute("xlink:title") == cardMesas[i].getAttribute("id")){
+                    cardMesas[i].classList.add("rest-emp-right-card-selected")
+                    cardMesas[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
+                }else{
+                    cardMesas[i].classList.remove("rest-emp-right-card-selected")
+                }
+            }
         }
         document.getElementById("rest-confirm-mesa").innerHTML = `
     <svg width="20px" height="20px" viewBox="0 0 24 24" fill="black" stroke="none" xmlns="http://www.w3.org/2000/svg">
@@ -289,6 +315,7 @@ function openPopup(){
     }, 10); // Un ligero retraso para que la transición funcione
 }
 
+
 function closePopup(){
     let div = document.getElementById("popup-reservar")
     // Eliminar la clase visible para iniciar la transición
@@ -301,7 +328,6 @@ function closePopup(){
 }
 
 function acceder() {
-    console.log("aquest click")
     document.querySelector(".rest-login").style.paddingLeft = "0px";
     setTimeout(() => {
         document.querySelector(".rest-login").innerHTML = `
@@ -316,15 +342,14 @@ function acceder() {
 }
 
 function profile() {
-    console.log("profile")
     if(document.getElementById("rest-web-map").style.display == "none"){
         document.getElementById("rest-web-profile").style.display = "none";
         document.getElementById("rest-web-map").style.display = "";
+        resizeWindow();
     }else{
         document.getElementById("rest-web-profile").style.display = "";
         document.getElementById("rest-web-map").style.display = "none";
     }
-    
 }
 
 inputFecha.addEventListener("change", () => {
@@ -374,9 +399,64 @@ function reservar() {
     if(reservas[`${inputFecha.value}|${inputHora.value}`] == undefined){
         reservas[`${inputFecha.value}|${inputHora.value}`] = [];
     }
-    reservas[`${inputFecha.value}|${inputHora.value}`].push(inputMesa.value);
+    const mesa = {
+        num: inputMesa.value,
+        name: "NAME",
+        lastname: "LASTNAME",
+        phone: "+34 999 999 999",
+        mail: "mail@mail.com",
+        people: inputPersonas.value
+    }
+    reservas[`${inputFecha.value}|${inputHora.value}`].push(mesa);
     console.log(reservas)
     buscarTaula()
+
+    const Content = `
+    <div class="rest-profile-card-left">
+      <div class="rest-profile-info">
+        <svg style="height: 50%;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        ${document.getElementById("rest-confirm-personas").textContent}
+      </div>
+      <div class="rest-profile-info">
+        <svg style="height: 50%;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 9H21M7 3V5M17 3V5M6 13H8M6 17H8M11 13H13M11 17H13M16 13H18M16 17H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+      </svg>
+      ${document.getElementById("rest-confirm-date").textContent}
+      </div>
+      <div class="rest-profile-info">
+        <svg style="height: 50%;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        ${document.getElementById("rest-confirm-time").textContent}
+      </div>
+      <div class="rest-profile-info">
+        <svg style="height: 50%;" width="20px" height="20px" viewBox="0 0 24 24" fill="black" stroke="none" xmlns="http://www.w3.org/2000/svg">
+          <path class="mesa-confirm" fill-rule="evenodd" clip-rule="evenodd" d="M1.73306 1.03631C2.08026 0.94014 2.45235 1.03817 2.7071 1.29292L22.7071 21.2929C23.0976 21.6834 23.0976 22.3166 22.7071 22.7071C22.3166 23.0977 21.6834 23.0977 21.2929 22.7071L14 15.4142L12.9142 16.5C12.1341 17.2802 10.8684 17.2827 10.0865 16.5007L3.29296 9.70718C1.58311 7.99734 1.01881 6.00221 0.879106 4.46556C0.809325 3.698 0.844063 3.03552 0.896734 2.56147C0.926579 2.29285 0.964429 2.02212 1.02938 1.75935C1.11559 1.41264 1.38869 1.13169 1.73306 1.03631ZM2.87096 4.2852C2.98137 5.49843 3.41721 7.00301 4.70713 8.29291L11.5 15.0858L12.5858 14L2.87096 4.2852Z" fill="none" stroke="none"></path> <path class="mesa-confirm" d="M18.2071 1.79292C18.5976 2.18344 18.5976 2.81661 18.2071 3.20713L15.3113 6.10295C14.955 6.45919 14.9505 6.97388 15.1985 7.38727L18.7929 3.79292C19.1834 3.40239 19.8166 3.40239 20.2071 3.79292C20.5976 4.18344 20.5976 4.81661 20.2071 5.20713L16.6128 8.80148C17.0261 9.04952 17.5408 9.04498 17.8971 8.68873L20.7929 5.79292C21.1834 5.40239 21.8166 5.40239 22.2071 5.79292C22.5976 6.18344 22.5976 6.81661 22.2071 7.20713L19.3113 10.1029C18.3379 11.0764 16.8269 11.2624 15.6465 10.5541L15.155 10.2592L14.7071 10.7071C14.3166 11.0977 13.6834 11.0977 13.2929 10.7071C12.9024 10.3166 12.9024 9.68344 13.2929 9.29292L13.7408 8.84501L13.4459 8.35354C12.7377 7.17311 12.9237 5.66214 13.8971 4.68873L16.7929 1.79292C17.1834 1.40239 17.8166 1.40239 18.2071 1.79292Z" fill="none" stroke="none"></path> <path class="mesa-confirm" d="M8.20711 17.2071C8.59763 16.8166 8.59763 16.1834 8.20711 15.7929C7.81659 15.4024 7.18342 15.4024 6.7929 15.7929L1.2929 21.2929C0.902372 21.6834 0.902372 22.3166 1.2929 22.7071C1.68342 23.0977 2.31659 23.0977 2.70711 22.7071L8.20711 17.2071Z" fill="none" stroke="none"></path> 
+        </svg>
+        ${document.getElementById("rest-confirm-mesa").textContent}
+      </div>
+      
+    </div>
+    <div class="rest-profile-card-right" >
+      <button style="height: 100%; cursor: pointer; background: transparent; border: none; border-radius: 50%;">
+      <svg style="height:80%" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18.3785 8.44975L11.4637 15.3647C11.1845 15.6439 10.8289 15.8342 10.4417 15.9117L7.49994 16.5L8.08829 13.5582C8.16572 13.1711 8.35603 12.8155 8.63522 12.5363L15.5501 5.62132M18.3785 8.44975L19.7927 7.03553C20.1832 6.64501 20.1832 6.01184 19.7927 5.62132L18.3785 4.20711C17.988 3.81658 17.3548 3.81658 16.9643 4.20711L15.5501 5.62132M18.3785 8.44975L15.5501 5.62132" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M5 20H19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      </button>
+      <button style="height: 100%; cursor: pointer; background: transparent; border: none;border-radius: 50%;">
+      <svg style="height:80%" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M14 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      </button>
+    </div>
+  `
+  const cardProduct = document.createElement("div");
+    cardProduct.classList.add("rest-profile-card")
+    cardProduct.innerHTML = Content;
+    document.getElementById("reservas").appendChild(cardProduct)
 }
 
 const mesasPorPersona = [[],["Mesa 16", "Mesa 22"],["Mesa 14","Mesa 3", "Mesa 6", "Mesa 8", "Mesa 11", "Mesa 4", "Mesa 13", "Mesa 18"],["Mesa 21","Mesa 23","Mesa 15","Mesa 17"],["Mesa 1","Mesa 2","Mesa 5","Mesa 9","Mesa 10","Mesa 19","Mesa 20"],["Mesa 7","Mesa 12"]]
@@ -406,22 +486,58 @@ function buscarTaula(){
         }
     }
 
+    const divCardMesas = document.querySelector(".rest-emp-right")
+    divCardMesas.innerHTML = "";
+
     if(reservas[`${inputFecha.value}|${inputHora.value}`] != undefined){
         for (let i = 0; i < reservas[`${inputFecha.value}|${inputHora.value}`].length; i++) {
             const element = reservas[`${inputFecha.value}|${inputHora.value}`][i];
+            const cardMesa = document.createElement("div");
+            cardMesa.classList.add("rest-emp-right-card");
+            cardMesa.setAttribute("id", element.num)
+            cardMesa.innerHTML = `
+            <h5 style="margin-left: 20px; margin-top: 20px; font-size: 18px; font-weight: 550;">${element.num.toUpperCase()}</h5>
+            <p style="margin-left: 30px; margin-top: 5px;">Nombre: ${element.name}</p>
+            <p style="margin-left: 30px; margin-top: 5px;">Apellidos: ${element.lastname}</p>
+            <p style="margin-left: 30px; margin-top: 5px;">Telefono: ${element.phone}</p>
+            <p style="margin-left: 30px; margin-top: 5px;">Mail: ${element.mail}</p>
+            <p style="margin-left: 30px; margin-top: 5px;">Personas: ${element.people}</p>
+            `
+            divCardMesas.appendChild(cardMesa);
+            cardMesa.addEventListener("click", () => {  
+                cardMesa.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                const cardMesas = document.querySelectorAll(".rest-emp-right-card");
+                for (let j = 0; j < cardMesas.length; j++) {
+                    cardMesas[j].classList.remove("rest-emp-right-card-selected")
+                }
+                for (let j = 0; j < mesas.length; j++) {
+                    if(mesas[j].parentElement.getAttribute("xlink:title") == cardMesa.id){
+                        mesas[j].classList.add("rest-mesa-selected")
+                    }else{
+                        mesas[j].classList.remove("rest-mesa-selected")
+                    }
+                }
+                cardMesa.classList.add("rest-emp-right-card-selected")
+            })
             for (let j = 0; j < mesas.length; j++) {
-                if(mesas[j].parentElement.getAttribute("xlink:title") == element){
-                    mesas[j].style.fill = "#80000036";
+                if(mesas[j].parentElement.getAttribute("xlink:title") == element.num){
+                    console.log(windowN)
+                    if(windowN == "client"){
+                        mesas[j].style.fill = "#80000036";
+                    }else{
+                        mesas[j].style.fill = "#00800036";
+                    }
                 }
             }
         }
     }
 
     mesas.forEach(elementP => {
-        console.log(elementP.parentElement.getAttribute("xlink:title"), inputMesa.value)
-        if(elementP.parentElement.getAttribute("xlink:title") == inputMesa.value){
+        if(elementP.style.fill == "rgba(128, 0, 0, 0.21)"){
+            elementP.classList.remove("rest-mesa-selected")
+            inputMesa.value = "";
+        }else if(elementP.parentElement.getAttribute("xlink:title") == inputMesa.value){
             elementP.classList.add("rest-mesa-selected")
-            console.log("hola?",elementP)
         }else{
             elementP.classList.remove("rest-mesa-selected")
         }
--- demo-rest-1/script.js
+++ demo-rest-1/script.js
@@ -3,7 +3,12 @@ const empresa = document.getElementById('window-empresa');
 const radioInputs = document.querySelectorAll('.radio-input label input[type="radio"]');
 const selection = document.querySelector('.selection');
 const containerWidth = 300;
+const restHStyle = document.getElementById("h-rest-stylesheet")
+const restVStyle = document.getElementById("v-rest-stylesheet")
+
 let horitzontal = true;
+let windowN = "client";
+let personesGuardades = 0;
 
 radioInputs.forEach((radio, index) => {
     radio.addEventListener('change', () => {
@@ -21,11 +26,65 @@ radioInputs.forEach((radio, index) => {
                 cliente.classList.remove('offscreen-right');
                 empresa.classList.add('offscreen-left');
                 empresa.classList.remove('center');
+                
+                windowN = "client";
+                document.querySelector(".button-rotate").setAttribute("for", "checkbox-rotate")
+
+                if(horitzontal){
+                    document.getElementById("checkbox-rotate").checked = false;
+                }else{
+                    document.getElementById("checkbox-rotate").checked = true;
+                }
+
+                inputPersonas.value = personesGuardades;
+                
+                const zooms = document.querySelectorAll(".rest-zoom-button");
+                setTimeout(() => {
+                    document.querySelector(".rest-map-bottom").appendChild(document.getElementById("svg"));
+                    document.querySelector("#client-filters").insertBefore(selectorHora,document.querySelector("#client-filters").children[1])
+                    document.querySelector("#client-filters").insertBefore(selectorFecha,document.querySelector("#client-filters").children[1])
+                    for (let i = 0; i < zooms.length; i++) {
+                        document.querySelector(".rest-map-bottom").appendChild(zooms[i])
+                    }
+                }, 100);
+                
+                resizeWindow();
+                buscarTaula();
+                
             }else if(radio.id == "input-empresa"){
                 empresa.classList.remove('offscreen-left');
                 empresa.classList.add('center');
                 cliente.classList.remove('center');
                 cliente.classList.add('offscreen-right');
+
+                windowN = "empresa";
+
+                personesGuardades = inputPersonas.value;
+                inputPersonas.value = 0;
+
+                document.getElementById("checkbox-rotate").checked = false;
+                console.log(document.getElementById("checkbox-rotate"))
+
+                document.querySelector(".button-rotate").setAttribute("for", "")
+
+                let a = horitzontal;
+                horitzontal = true;
+
+                const zooms = document.querySelectorAll(".rest-zoom-button")
+                setTimeout(() => {
+                    document.querySelector("#aqui-svg").appendChild(document.getElementById("svg"));
+                    document.querySelector("#aqui-filters").appendChild(selectorFecha);
+                    document.querySelector("#aqui-filters").appendChild(selectorHora);
+                    for (let i = 0; i < zooms.length; i++) {
+                        document.querySelector("#aqui-svg").appendChild(zooms[i])
+                    }
+                }, 100);
+
+                resizeWindow();
+
+                buscarTaula();
+
+                horitzontal = a;
             }
         }
     });
@@ -69,21 +128,22 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 document.getElementById("checkbox-rotate").addEventListener('change', () => {
-    const windows = document.querySelectorAll(".div-window");
-    if (document.getElementById("checkbox-rotate").checked) {
-        horitzontal = false;
-        resizeWindow();
-    }else{
-        horitzontal = true;
-        resizeWindow();
+    if(windowN == "client"){
+        if (document.getElementById("checkbox-rotate").checked) {
+            horitzontal = false;
+            resizeWindow();
+        }else{
+            horitzontal = true;
+            resizeWindow();
+        }
     }
+    
 });
 
 window.addEventListener('resize', resizeWindow);
 window.addEventListener('load', resizeWindow)
 
-const restHStyle = document.getElementById("h-rest-stylesheet")
-const restVStyle = document.getElementById("v-rest-stylesheet")
+
 
 function resizeWindow(){
     const windows = document.querySelectorAll(".div-window")
@@ -103,17 +163,31 @@ function resizeWindow(){
                 restVStyle.setAttribute('disabled', 'true')
             }, 100);
         }else{
-            if(heightDiv*9/16 > widthDiv){
-                window.style.width = `${widthDiv}px`;
-                window.style.height = `${widthDiv*16/9}px`;
-            }else{
-                window.style.width = `${heightDiv*9/16}px`;
-                window.style.height = `${heightDiv}px`;
+            if(window.id == "window-empresa"){
+                if(heightDiv*16/9 > widthDiv){
+                    window.style.width = `${widthDiv}px`;
+                    window.style.height = `${widthDiv*9/16}px`;
+                }else{
+                    window.style.width = `${heightDiv*16/9}px`;
+                    window.style.height = `${heightDiv}px`;
+                }
+                setTimeout(() => {
+                    restVStyle.removeAttribute('disabled')
+                    restHStyle.setAttribute('disabled', 'true')
+                }, 100);
+            }else {
+                if(heightDiv*9/16 > widthDiv){
+                    window.style.width = `${widthDiv}px`;
+                    window.style.height = `${widthDiv*16/9}px`;
+                }else{
+                    window.style.width = `${heightDiv*9/16}px`;
+                    window.style.height = `${heightDiv}px`;
+                }
+                setTimeout(() => {
+                    restHStyle.removeAttribute('disabled')
+                    restVStyle.setAttribute('disabled', 'true')
+                }, 100);
             }
-            setTimeout(() => {
-                restVStyle.removeAttribute('disabled')
-            restHStyle.setAttribute('disabled', 'true')
-            }, 100);
             
         }
     })
@@ -124,8 +198,43 @@ function viewBoxSvg() {
     document.querySelector(".loader").style.display = "block";
     svg.setAttribute("viewBox", `0 0 0 0`)
     setTimeout(() => {
-        console.log(svg.clientHeight, svg.clientWidth)
-        svg.setAttribute("viewBox", `-160 -290 ${svg.clientWidth*2-10} ${svg.clientHeight*2-10}`)
+        let widthSVG = (svg.parentElement.clientWidth - 1410)/2;
+        let heightSVG = (svg.parentElement.clientHeight - 980)/2;
+
+        const container = svg.parentElement;
+        
+        const containerWidth = container.clientWidth;
+        const containerHeight = container.clientHeight;
+  
+        const svgWidth = 1370;
+        const svgHeight = 800;
+  
+        // Calcula la relación de aspecto del contenedor y del SVG
+        const containerAspectRatio = containerWidth / containerHeight;
+        const svgAspectRatio = svgWidth / svgHeight;
+  
+        let newViewBoxWidth, newViewBoxHeight;
+  
+        // Si el contenedor es más ancho proporcionalmente que el SVG
+        if (containerAspectRatio > svgAspectRatio) {
+          // Ajustar el ancho del SVG para que el alto se ajuste al contenedor
+          newViewBoxHeight = svgHeight;
+          newViewBoxWidth = svgHeight * containerAspectRatio;
+        } else {
+          // Ajustar el alto del SVG para que el ancho se ajuste al contenedor
+          newViewBoxWidth = svgWidth;
+          newViewBoxHeight = svgWidth / containerAspectRatio;
+        }
+  
+        // Calcula el desplazamiento para centrar el SVG en el viewBox
+        const offsetX = (newViewBoxWidth - svgWidth) / 2;
+        const offsetY = (newViewBoxHeight - svgHeight) / 2;
+  
+        // Modifica el viewBox con los nuevos valores, centrando el SVG
+        svg.setAttribute('viewBox', `${-140 - 50 - offsetX} ${ -280 - 50 - offsetY} ${newViewBoxWidth} ${newViewBoxHeight}`);
+  
+
+        //svg.setAttribute("viewBox", `${-140 - 10 - widthSVG} ${-280 - 10 - heightSVG} ${svg.parentElement.clientWidth} ${svg.parentElement.clientHeight}`)
         document.querySelector(".loader").style.display = "none";
     }, 600);
 }
