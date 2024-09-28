00dc490961b1cb90ce2553855d12d6d4eebf489a
llorenccodinach <llorenc.codinach@gmail.com>
2024-09-28 12:23:48 +0200

    index inicial

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function p(){
-    console.log("p")
-    document.getElementById("proximamente").scrollIntoView({ behavior: 'smooth', block: 'center' });
-}
-
-function r(){
-    console.log("r")
-    document.getElementById("restauracion").scrollIntoView({ behavior: 'smooth', block: 'center' });
-}
-
 window.addEventListener("load", () => {
     console.log("hi",document.getElementById("proximamente"))
     document.getElementById("proximamente").scrollIntoView({ behavior: 'smooth', block: 'center' });
+    document.querySelector("body").style.overflow = 'hidden';
 })
+
+
+window.addEventListener('scroll', function() {
+    this.window.scroll(0,0);
+    this.window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth' // O 'auto' para un desplazamiento instantáneo
+      });
+    console.log("ho fa ")
+    setTimeout(() => {
+        this.window.scroll(0,0);
+        this.window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth' // O 'auto' para un desplazamiento instantáneo
+          });
+    }, 100);
+});
+
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault(); // Previene el scroll en pantallas táctiles
+  }, { passive: false });
