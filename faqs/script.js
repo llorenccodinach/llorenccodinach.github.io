dfb4e83f1a954ead47850e2a2809217b6f0d0b06
llorenccodinach <llorenc.codinach@gmail.com>
2024-10-02 12:53:04 +0200

    index inicial

--- faqs/script.js
+++
@@ -1,9 +0,0 @@
-function prova(){
-    if(document.getElementById("bottom").classList.contains("active")){
-        document.getElementById("bottom").classList.remove("active");
-        document.getElementById("question").classList.remove("active")
-    }else{
-        document.getElementById("bottom").classList.add("active");
-        document.getElementById("question").classList.add("active")
-    }
-}
